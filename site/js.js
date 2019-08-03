6418164d0086ea604e7ff8045adff86eb52a28d2
gabrielhp11 <gabrielhenriquep123@gmail.com>
2019-08-03 14:39:27 -0300

    first commit

---
+++ site/js.js
@@ -0,0 +1,7 @@
+function red() {
+    <form method="POST" a href="" target="_blank" action="https://www.paypal.com/br/home">
+  
+    <input type="submit" value="Submeter" name="B3"></p>
+    </form>
+
+}
